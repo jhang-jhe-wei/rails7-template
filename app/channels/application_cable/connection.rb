f4a7f052abefd02d9f902c9bdaa88071c373121a
wells <jhang0912407249@gmail.com>
2024-01-06 17:28:25 +0800

    fix: upgrade error

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,13 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
 
 module ApplicationCable
-  class ConnectionTest < ActionCable::Connection::TestCase
-    # test "connects with cookies" do
-    #   cookies.signed[:user_id] = 42
-    #
-    #   connect
-    #
-    #   assert_equal connection.user_id, "42"
-    # end
+  class Connection < ActionCable::Connection::Base
   end
 end
